616ebba3c0767d9e0f17491eff0f5997b52c27b4
cdmorozov <kurumabetabeta@gmail.com>
2026-08-01 18:49:38 +0300

    fix(server): add global api prefix

--- server/src/main.ts
+++ server/src/main.ts
@@ -5,6 +5,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.use(cookieParser());
   app.enableCors({ origin: process.env.WEB_ORIGIN, credentials: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
